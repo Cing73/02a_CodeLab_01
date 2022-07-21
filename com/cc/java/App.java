4f4b116e15c0b41a188b3193da5d0c134f105d18
Cing73 <craig@vancleaf>
2022-07-21 08:54:29 +0200

    erste losung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,22 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    
+        Konto konto1 = new Konto();
+        System.out.println("Konto 1 aktuell: " + konto1.getKontostand());
+
+        Konto konto2 = new Konto();
+        System.out.println("Konto 2 aktuell: " + konto2.getKontostand());
+
+        Konto konto3 = new Konto();
+        System.out.println("Konto 3 aktuell: " + konto3.getKontostand());
+
+        System.out.println("Konto 1 verdoppelt: " + konto1.getKontostand() * 2);
+
+        System.out.println("Konto 2 verdreifacht: " + konto2.getKontostand() * 3);
+
+        System.out.println("Konto 3 verzehnfacht: " + konto3.getKontostand() * 10);
+
     }
 
     
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -2,4 +2,21 @@ package com.cc.java;
 
 public class Konto {
     
+    private int kontostand = 10000;
+
+    // public Konto(int kontostand) {
+    //     this.kontostand = kontostand;
+
+    // }
+    public Konto() {
+            }
+
+
+        public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand (int kontostand) {
+        this.kontostand = kontostand;
+    }
 }
