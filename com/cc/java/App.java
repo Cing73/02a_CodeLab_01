f28bfeb84c1f6ac62de5e9044237bc9d7f29a167
Cing73 <craig@vancleaf>
2022-07-21 10:30:56 +0200

    end

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,24 +4,25 @@ public class App {
     
     public static void main(String[] args) {
     
-        Konto konto1 = new Konto();
-        System.out.println("Konto 1 aktuell: " + konto1.getKontostand());
+        Konto konto1 = new Konto(10000);
+            System.out.println("Konto1 = " + konto1.getKontostand() + " €");
+        
+        Konto konto2 = new Konto(10000);
+            System.out.println("Konto2 = " + konto2.getKontostand() + " €");
 
-        Konto konto2 = new Konto();
-        System.out.println("Konto 2 aktuell: " + konto2.getKontostand());
+        Konto konto3 = new Konto(10000);
+            System.out.println("Konto3 = " + konto3.getKontostand() + " €");  
+    
+        System.out.println("-----------------------------");
 
-        Konto konto3 = new Konto();
-        System.out.println("Konto 3 aktuell: " + konto3.getKontostand());
+        konto1.setKontostand(konto1.getKontostand()*2);
+        konto2.setKontostand(konto2.getKontostand()*3);
+        konto3.setKontostand(konto3.getKontostand()*10);
 
-        System.out.println("Konto 1 verdoppelt: " + konto1.getKontostand() * 2);
-
-        System.out.println("Konto 2 verdreifacht: " + konto2.getKontostand() * 3);
-
-        System.out.println("Konto 3 verzehnfacht: " + konto3.getKontostand() * 10);
+        System.out.println("Konto1 = " + konto1.getKontostand() + " €");
+        System.out.println("Konto2 = " + konto2.getKontostand() + " €");
+        System.out.println("Konto3 = " + konto3.getKontostand() + " €");
 
     }
-
-    
-
 }
 
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -2,21 +2,17 @@ package com.cc.java;
 
 public class Konto {
     
-    private int kontostand = 10000;
+    private int kontostand;
 
-    // public Konto(int kontostand) {
-    //     this.kontostand = kontostand;
+    public Konto(int kontostand) {
+        this.kontostand = kontostand;
 
-    // }
-    public Konto() {
-            }
-
-
-        public int getKontostand() {
+    }
+    public int getKontostand() {
         return kontostand;
     }
 
-    public void setKontostand (int kontostand) {
+    public void setKontostand(int kontostand) {
         this.kontostand = kontostand;
     }
 }
